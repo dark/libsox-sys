84c64b51785052f35f27b310f972246e9501bd3f
Vincent Tavernier <vince.tavernier@gmail.com>
2019-04-25 22:28:53 +0200

    Configure custom docs hosting using gh-pages

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#![doc(html_root_url = "https://vtavernier.github.io/libsox-sys/")]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
