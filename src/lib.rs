3c89f97687261a038231fbc185c22a3ee5dc533c
Vincent Tavernier <vince.tavernier@gmail.com>
2019-04-25 21:43:33 +0200

    Add bindgen-generated wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn format_init() {
+        unsafe {
+            sox_format_init();
+            sox_format_quit();
+        }
     }
 }
